124418b66826c2e13f1a163993a0df1072d01d6a
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-05-17 22:53:09 +0300

    step 3 -7

--- customer.py
+++ customer.py
@@ -1,5 +1,41 @@
+from order import Order
+
 class Customer:
     
     def __init__(self, name):
         self.name = name
         
+@property
+def name(self):
+    return self._name
+
+@name.setter
+def name(self, value):
+    if isinstance(value, str) and 1 <= len(value) <= 15:
+        self._name = value
+    else:
+        raise ValueError("Name must be a string between 1 and 15 characters.")       
+    
+    
+def orders(self):
+    return [order for order in Order.all() if order.cutomer ==self]
+
+def coffees(self):
+    return list(set(order.coffee for order in self.orders()))
+
+def create_order(self, coffee, price):
+    from order import Order
+    return Order(self, coffee, price)
+
+
+@classmethod
+
+def most_aficionado(cls, coffee):
+    customer_spending = {}
+    for order in Order.all():
+        if order.coffee ==coffee:
+            customer_spending[order.customer] = customer_spending.get(order.customer, 0) + order.price
+    if customer_spending:
+        return max(customer_spending, key=customer_spending.get)
+    return None       
+    
--- order.py
+++ order.py
@@ -1,4 +1,58 @@
 class Order:
     
-    def __init__(self, name):
-        self.name = name
+    _all =[]  #to keep track of all orders
+    
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+        Order._all.append(self)
+        
+        
+    @property
+    def customer(self):
+        return self._customer    
+    
+    
+    
+    @customer.setter
+    def customer(self, value):
+        from customer import Customer
+        if isinstance(value, Customer):
+            self._customer = value
+        else:
+            raise ValueError("Customer must be a Customer instance.")
+        
+        
+    @property
+    def coffee(self):
+        return self._coffee    
+    
+    @coffee.setter
+    def coffee(self, value):
+        from coffee import Coffee
+        if isinstance(value, Coffee):
+            self._coffee = value
+            
+        else:
+            raise ValueError("Coffee must be a Coffee instance.")
+        
+        
+        
+    @property
+    def price(self):
+        return self._price   
+    
+    
+    @price.setter
+    def price(self, value):
+        if isinstance(value, float) and 1.0 <= value <= 10.0:
+            self._price = value
+            
+        else:
+            raise ValueError("Price must be a float between 1.0 and 10.0.")
+        
+        
+    @classmethod #give access to all orders
+    def all(cls):
+        return cls._all        
